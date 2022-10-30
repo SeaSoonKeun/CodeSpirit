978fc69f447ea30210a32ea10749258d185a4f18
wuzhi <1123442688@qq.com>
2022-10-31 01:36:53 +0800

    class01 code

--- 05_practice/src/com/gomboclabs/codespirit/practice/improve/class01/Code01_CordCoverMaxPoint.java
+++ 05_practice/src/com/gomboclabs/codespirit/practice/improve/class01/Code01_CordCoverMaxPoint.java
@@ -1,17 +1,45 @@
 package com.gomboclabs.codespirit.practice.improve.class01;
 
-import java.util.ArrayList;
+
+import org.junit.Test;
+
+import java.util.Arrays;
 
 public class Code01_CordCoverMaxPoint {
     public static void main(String[] args) {
-        int[] dotList = new int[]{210, 255, 273, 300, 304, 382, 717, 739, 938};
-        int k = 977;
-        System.out.println(cordCoverMaxPoint(dotList, k));
+        int len = 100;
+        int max = 1000;
+        int testTime = 1000;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTime; i++) {
+            int k = (int) (Math.random() * max);
+            int[] dotList = generateArray(len, max);
+            int ans1 = cordCoverMaxPoint1(dotList, k);
+            int ans2 = cordCoverMaxPoint2(dotList, k);
+            int ans3 = cordCoverMaxPoint3(dotList, k);
+            int ans4 = test(dotList, k);
+            if (ans1 == ans2 || ans2 == ans3 || ans3 == ans4) {
+                    System.out.println("Congratulations!!!");
+            } else {
+                System.out.println("dotList : [");
+                for (int i1 : dotList) {
+                    System.out.printf(i1 + ", ");
+                }
+                System.out.println("]");
+                System.out.println("k :" + k);
+                System.out.println(ans1);
+                System.out.println(ans2);
+                System.out.println(ans3);
+                System.out.println(ans4);
+                System.out.println("ooops, try again");
+            }
+        }
     }
 
-    public static int cordCoverMaxPoint(int[] dotList, int k) {
+
+    // 暴力for循环
+    public static int cordCoverMaxPoint1(int[] dotList, int k) {
         int maxCoverNum = 1;
-        ArrayList<Integer> startPointIndex = new ArrayList<>();
         for (int i = 0; i < dotList.length; i++) {
             int curCoverNum = 1;
             for (int j = i; j < dotList.length - 1; j++) {
@@ -19,22 +47,97 @@ public class Code01_CordCoverMaxPoint {
                     curCoverNum++;
                     if (curCoverNum > maxCoverNum) {
                         maxCoverNum = curCoverNum;
-                        startPointIndex = new ArrayList<>();
-                        startPointIndex.add(i);
-                    }else if (curCoverNum == maxCoverNum){
-                        startPointIndex.add(i);
                     }
-                }else {
+                } else {
                     break;
                 }
             }
         }
-//        System.out.println("覆盖最多点位时，绳子的起始点包含：");
-//        for (Integer pointIndex : startPointIndex) {
-//            System.out.println(pointIndex);
-//        }
         return maxCoverNum;
     }
+
+    // 二分法找指定位置
+    @Test
+    public static int cordCoverMaxPoint2(int[] dotList, int k) {
+//    public void cordCoverMaxPoint2() {
+//        int[] dotList = new int[]{205, 284, 559, 615, 726};
+//        int k = 220;
+        int maxCoverNum = 1;
+        int left = 0;
+        int L;
+        int R = dotList.length - 1;
+        for (; left < dotList.length; left++) {
+            L = left;
+            R = dotList.length - 1;
+            // 注意此处需要两个括号
+            int Mid = L + ((R - L) >> 1);
+            while (L <= R) {
+                Mid = L + ((R - L) >> 1);
+                // 能压到最远的值在mid右边
+                if (dotList[Mid] - dotList[L] <= k) {
+                    L = Mid + 1;
+                }
+                // 能压到最远的值在mid左边
+                else {
+                    R = Mid - 1;
+                }
+            }
+            // 此处Mid已经超出最远能够到的右边界，所以需要-1
+            maxCoverNum = Math.max(Mid - left + 1, maxCoverNum);
+        }
+        return maxCoverNum;
+//        System.out.println(maxCoverNum);
+    }
+
+    @Test
+    // 双指针（滑动窗口）
+    public static int cordCoverMaxPoint3(int[] dotList, int k) {
+//    public void cordCoverMaxPoint3() {
+//        int[] dotList = new int[]{205, 284, 559, 615, 726};
+//        int k = 220;
+        int cordMaxNum = 1;
+        int L = 0;
+        // R 指针其实位置也为0，因为不一定能覆盖到
+        int R = 0;
+        // L从0往右移
+        for (; L < dotList.length; L++) {
+            // R不越界 且 绳子能压住R所在的点，R往右移
+            while (L < dotList.length && R < dotList.length && dotList[R] - dotList[L] <= k) {
+                R++;
+            }
+            // R点绳子压不住，R回退一点
+            R = R - 1;
+            // 比较当前R和L距离与目前最大距离，取最大值
+            // 此处要注意，下标是从0开始的，所以需要+1
+            cordMaxNum = Math.max(cordMaxNum, R - L + 1);
+        }
+        return cordMaxNum;
+//        System.out.println(cordMaxNum);
+    }
+
+    // for test
+    public static int test(int[] arr, int L) {
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int pre = i - 1;
+            while (pre >= 0 && arr[i] - arr[pre] <= L) {
+                pre--;
+            }
+            max = Math.max(max, i - pre);
+        }
+        return max;
+    }
+
+    // for test
+    public static int[] generateArray(int len, int max) {
+        int[] ans = new int[(int) (Math.random() * len) + 1];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = (int) (Math.random() * max);
+        }
+        Arrays.sort(ans);
+        return ans;
+    }
+
 }
 
 
